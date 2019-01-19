4a1898182e03faf2faf998d44fff75715121e629
Sivem17 <36225367+Sivem17@users.noreply.github.com>
2019-01-18 22:23:36 -0400

    All Assignments

--- A01/PovertyData.java
+++ A01/PovertyData.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package a01;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 /**
+ * 
  *
  * @author Aitezaz Siddiqi (A00431079)
  */
@@ -19,19 +15,20 @@ public class PovertyData {
      */
     public static void main(String[] args) {
         Scanner kbd = new Scanner(System.in);
-
+        int numFamilies;
         double[] incomeData;
         double minimumIncome;
 
         printIntroduction();
-        incomeData = readIncomeData(kbd);
-        minimumIncome = readMinimumIncome(kbd);
-        printPause(kbd);
+        numFamilies = getNumberOfFamilies();
+        incomeData = getIncomeData( numFamilies);
+        minimumIncome = getMinimumIncome();
+        printPause();
         printReport(incomeData, minimumIncome);
 
     }
-
-    public static void printIntroduction() {
+    private static void printIntroduction() {
+        //Prints the required introduction
         System.out.println("A00431079 -- Siddiqi, Aitezaz\n"
                 + "CSCI 1228 -- Winter 2019\n"
                 + "A01 -- due 2019-01-18\n\n"
@@ -41,11 +38,11 @@ public class PovertyData {
                 + "report on poverty\n"
                 + "rates among the families.\n");
     }
+    private static int getNumberOfFamilies() {
 
-    public static double[] readIncomeData(Scanner kbd1) {
-
+        
+        Scanner kbd1 = new Scanner(System.in);
         int numFamilies;
-        double familyIncome;
 
         System.out.print("How many families are there in this country? ");
         numFamilies = kbd1.nextInt();
@@ -57,93 +54,91 @@ public class PovertyData {
             kbd1.nextLine();
         }
         System.out.println();
-
+        return numFamilies;
+    }
+    private static double[] getIncomeData( int numFamilies) {
+        
+        Scanner kbd2 = new Scanner(System.in);
         double[] array = new double[numFamilies];
-        for (int i = 1; i <= numFamilies; ++i) {
-            System.out.print("What is the income for family #" + i + "? ");
-            familyIncome = kbd1.nextDouble();
-            kbd1.nextLine();
-            while (familyIncome < 0) {
+        for (int i = 0, j = 1; i < numFamilies; ++i, ++j) {
+            System.out.print("What is the income for family #" + j + "? ");
+            array[i] = kbd2.nextDouble();
+            kbd2.nextLine();
+            //While loop for taking in valid input.
+            while (i < 0) {
                 System.out.println("The income must be non-negative.");
                 System.out.print("What is the income for family #"
-                        + i + "? ");
-                familyIncome = kbd1.nextDouble();
-                kbd1.nextLine();
+                        + j + "? ");
+                array[i] = kbd2.nextDouble();
+                kbd2.nextLine();
             }
         }
         System.out.println();
         return array;
     }
-
-    public static double readMinimumIncome(Scanner kbd2) {
-
+    private static double getMinimumIncome() {
+        
+        Scanner kbd3 = new Scanner(System.in);
         double minIncome;
         System.out.print("What is the minimum income required to "
                 + "support a family? ");
-        minIncome = kbd2.nextDouble();
-        kbd2.nextLine();
+        minIncome = kbd3.nextDouble();
+        kbd3.nextLine();
         while (minIncome <= 0) {
             System.out.println("The income must be positive.");
             System.out.print("What is the minimum income required to "
                     + "support a family? ");
-            minIncome = kbd2.nextDouble();
-            kbd2.nextLine();
+            minIncome = kbd3.nextDouble();
+            kbd3.nextLine();
         }
         return minIncome;
     }
-
-    public static void printPause(Scanner kbd3) {
-
+    private static void printPause() {
+        
+        Scanner kbd3 = new Scanner(System.in);
         System.out.println();
         System.out.print("Press Enter...");
         kbd3.nextLine();
         System.out.println();
     }
-
-    public static void printReport(double[] array, double basicIncome) {
+    private static void printReport(double[] array, double basicIncome) {
         System.out.println("Report\n"
                 + "------\n"
                 + "Average Income:        $" + getAverage(array) + "\n"
                 + "Basic Needs Income:    $" + basicIncome + "\n"
-                + "Absolute Poverty Rate:  " + getPovertyRate(array, basicIncome));
+                + "Absolute Poverty Rate:  "
+                + getPovertyRate(array, basicIncome));
 
         System.out.println("Report\n"
                 + "------\n"
                 + "Median Income:         $" + getMedian(array) + "\n"
-                + "Relative Needs Income: $" + getRelativeNeedsIncome(array) + "\n"
-                + "Relative Poverty Rate:  " + getPovertyRate(array, getRelativeNeedsIncome(array)));
+                + "Relative Needs Income: $" + getRelativeNeedsIncome(array)
+                + "\n" + "Relative Poverty Rate:  "
+                + getPovertyRate(array, getRelativeNeedsIncome(array)) + "%");
     }
-
-    public static double getPovertyRate(double[] array, double income) {
-
-        int familyInPoverty = 0;
-        double result;
+    private static double getPovertyRate(double[] array, double income) {
+        double familyInPoverty = 0;
 
         for (int i = 0; i < array.length; ++i) {
             if (array[i] < income) {
                 familyInPoverty++;
             }
         }
-        result = familyInPoverty / array.length;
-
-        return result;
+        return (familyInPoverty / array.length) * 100;
     }
-
-    public static double getRelativeNeedsIncome(double[] array) {
+    private static double getRelativeNeedsIncome(double[] array) {
         return 0.6 * getMedian(array);
     }
-
-    public static double getAverage(double[] i) {
+    private static double getAverage(double[] i) {
         double sum = 0, average;
 
-        for (double j : i) {
-            sum += j;
+        for (int j = 0; j < i.length; j++) {
+            sum = sum + i[j];
         }
         average = sum / i.length;
         return average;
     }
-
-    public static double getMedian(double[] i) {
+    private static double getMedian(double[] i) {
         double[] j = Arrays.copyOf(i, i.length);
         Arrays.sort(j);
         if (j.length % 2 == 0) {
