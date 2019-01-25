52eb059a6d05db5dfe5fae22357afd486829373c
Sivem17 <36225367+Sivem17@users.noreply.github.com>
2019-01-24 21:13:01 -0400

    All Assignments

--- A02/RegularPolygon.java
+++ A02/RegularPolygon.java
@@ -5,10 +5,8 @@ package a02;
  * @author Aitezaz Siddiqi
  */
 public class RegularPolygon {
-
     public final int numOfSides;
     public final double lengthOfSides;
-
     RegularPolygon(int numSides, double lengthSides) {
         if (numSides < 0) {
             numOfSides = 0;
@@ -21,22 +19,18 @@ public class RegularPolygon {
             lengthOfSides = lengthSides;
         }
     }
-
     public final int getNumOfSides() {
         return numOfSides;
     }
-
     public final double getLengthOfSides() {
         return lengthOfSides;
     }
-
-    public final double getPerimeter() {
+    public double getPerimeter() {
         return numOfSides * lengthOfSides;
     }
-
-    public final double getArea() {
-        return ((double) numOfSides * Math.pow(lengthOfSides, 2)
-                / (4.00 * (double) Math.tan(Math.PI / numOfSides)));
+    public double getArea() {
+        return ((double) numOfSides * (double) Math.pow(lengthOfSides, 2)
+                / ((double) 4 * (double) Math.tan(Math.PI / numOfSides)));
     }
 
 }
