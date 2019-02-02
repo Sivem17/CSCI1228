75448650c2a82ffdf5529f8e1a6ea5e2d8c3a8c6
Sivem17 <36225367+Sivem17@users.noreply.github.com>
2019-02-01 23:47:13 -0400

    All Assignments

--- A03/SongPlayer.java
+++ A03/SongPlayer.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package a03;
 
 import java.util.Arrays;
@@ -23,11 +18,9 @@ public class SongPlayer {
         Song[] song = new Song[20];
         String songTitle, songLength;
 
-//        Song newSong = new Song(songTitle, songLength);
         int numSongs = 0;
 
         printIntroduction();
-//        getTitleAndLength(kbd, song, newSong);
 
         for (int i = 0; i < song.length; ++i) {
 
@@ -42,36 +35,39 @@ public class SongPlayer {
             song[i] = new Song(songTitle, songLength);
             numSongs++;
         }
-//        makeShuffleOrder(numSongs - 1);
-        System.out.println(Arrays.toString(makeShuffleOrder(numSongs)));
 
         int[] shuffledOrder = makeShuffleOrder(numSongs);
-//        for (int i = 0; i < song.length; ++i) {
-//             song[shuffledOrder[i]].play();
-//        }
-//        System.out.println("Now Playing: ");
         for (int i = 0; i < numSongs; ++i) {
             song[shuffledOrder[i]].play();
         }
 
     }
 
+    /**
+     * Prints the introduction.
+     */
     private static void printIntroduction() {
 
         System.out.println("Song Player\n"
                 + "-----------\n\n"
                 + "Aitezaz Siddiqi (A00431079)\n\n"
-                + "This Player can hold up to 20 songs, and plays them in a random order.\n\n"
+                + "This Player can hold up to 20 songs, and plays them in a "
+                + "random order.\n\n"
                 + "Enter the song titles and lengths below. "
                 + "Leave the title blank to end early.\n");
     }
 
+    /**
+     * A method which shuffles the order.
+     *
+     * @param howMany Number of Songs
+     * @return
+     */
     private static int[] makeShuffleOrder(int howMany) {
 
         int temp;
         int[] songs = new int[howMany];
 
-        
         for (int i = 0; i < songs.length; ++i) {
             songs[i] = i;
         }
@@ -82,30 +78,8 @@ public class SongPlayer {
             temp = songs[i];
             songs[i] = songs[swapLoc];
             songs[swapLoc] = temp;
-            
-            System.out.println(i + "=" + temp + " with " + swapLoc + "=" + songs[i]);
         }
         return songs;
     }
 
-//    private static void getTitleAndLength(Scanner kbd1, Song[] songs, Song soo) {
-//        String songTitle, songLength;
-//
-//        int numSongs = 0;
-//        System.out.print("Enter song title: ");
-//        songTitle = kbd1.nextLine();
-//
-//        for (int i = 0; i < songs.length; ++i) {
-//
-//            while (!((i == songs.length) || (songTitle.isEmpty()))) {
-//                System.out.print("Enter song length (e.g. 3:07): ");
-//                songLength = kbd1.nextLine();
-//                soo = new Song(songTitle, songLength);
-//                numSongs++;
-//                System.out.print("Enter song title: ");
-//                songTitle = kbd1.nextLine();
-//            }
-//
-//        }
-//    }
 }
