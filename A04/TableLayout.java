e0e8ec211b4502327c7832ef8fa73f2eeb4af235
Sivem17 <36225367+Sivem17@users.noreply.github.com>
2019-02-08 22:28:20 -0400

    All Assignments

--- A04/TableLayout.java
+++ A04/TableLayout.java
@@ -5,43 +5,38 @@ import javafx.scene.layout.GridPane;
 
 /**
  *
- * @author Andrew Boutres(A00428722)
+ * @author Aitezaz Siddiqi (A00431079)
  */
 public class TableLayout extends GridPane {
 
     public final int NUM_ROWS;
     public final int NUM_COLUMNS;
-    
+
     private int numRows = 0;
     private int numColumns = 0;
 
-    
     public TableLayout(int rows, int columns) {
         if (rows < 1) {
             NUM_ROWS = 1;
-        } 
-        else {
+        } else {
             NUM_ROWS = rows;
         }
         if (columns < 1) {
             NUM_COLUMNS = 1;
-        }
-        else {
+        } else {
             NUM_COLUMNS = columns;
         }
     }
 
-   
     public void add(Node ns1) {
-        
+
         if (numColumns < NUM_COLUMNS) {
             super.add(ns1, numColumns, numRows);
             ++numColumns;
-        } 
-        else {
+        } else {
             ++numRows;
             numColumns = 0;
-            
+
             if (numRows < NUM_ROWS) {
                 super.add(ns1, numColumns, numRows);
                 ++numColumns;
@@ -50,17 +45,15 @@ public class TableLayout extends GridPane {
 
     }
 
-    
     public void add(Node ns2, int columns) {
-        
+
         if (numColumns < NUM_COLUMNS) {
             super.add(ns2, numColumns, numRows, columns, 1);
             numColumns += columns;
-        } 
-        else {
+        } else {
             ++numRows;
             numColumns = 0;
-            
+
             if (numColumns < NUM_COLUMNS) {
                 super.add(ns2, numColumns, numRows,
                         columns, 1);
