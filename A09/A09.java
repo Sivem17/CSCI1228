aa76da3ae34c841fa3742b4753ad29da9676e20a
Sivem17 <36225367+Sivem17@users.noreply.github.com>
2019-03-29 23:15:30 -0300

    Update A09.java

--- A09/A09.java
+++ A09/A09.java
@@ -1,12 +1,11 @@
-package a09;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 /**
+ * This program reads and answers the request from the user also it handles the
+ * errors using exception but the program does not end
  *
- * @author Aitezaz Siddiqi (A00431079)
+ * @author s3650728
  */
 public class A09 {
 
@@ -15,110 +14,125 @@ public class A09 {
      */
     public static void main(String[] args) {
 
-        Scanner kbd = new Scanner(System.in);
-
-        String result;
-        int number1, number2;
-        List<Integer> list = new ArrayList();
-
+        //Introduce yourself
         printIntroduction();
 
-        System.out.print(">>> ");
-        result = kbd.next();
-        while (!(result == null)) {
-            if (result.equalsIgnoreCase("HELP")) {
-                System.out.println("\nValid commands are:\n"
-                        + " - factorial #\n"
-                        + "     The product of all numbers from 1 to #.\n"
-                        + "     (The argument must not be negative.)\n\n"
-                        + " - gcd # #\n"
-                        + "     The greatest common divisor of the two numbers.\n"
-                        + "     The biggest number that divides evenly into both "
-                        + "of them.\n\n"
-                        + " - sorted #...\n"
-                        + "     Whether the numbers are in order from smallest "
-                        + "to largest.\n"
-                        + "     If not, then where the first out-of-order number "
-                        + "is.\n\n"
-                        + " - help\n"
-                        + "     This help message.\n\n"
-                        + " - quit\n"
-                        + "     End the program.\n\n");
-            } else if (result.equalsIgnoreCase("factorial")) {
-                number1 = kbd.nextInt();
-                kbd.nextLine();
-                System.out.println(number1 + "! == " + getFactorial(number1));
-            } else if (result.equalsIgnoreCase("gdc")) {
-                number1 = kbd.nextInt();
-                number2 = kbd.nextInt();
-                kbd.nextLine();
-                System.out.println("gcd(" + number1 + ", " + number2 + ") == " + greatestCommonDivisor(number1, number2));
-            } else if (result.equalsIgnoreCase("sorted")) {
-//                number1 = kbd.nextInt();
-//                kbd.nextLine();
-                while (kbd.hasNextInt()) {
-//                    number2 = kbd.nextInt();                    
-//                    kbd.nextLine();
-//                    if (number1 <= number2) {
-//                        System.out.println("That list is sorted.");
-//                    } else {
-//                        System.out.println("Out of order");
-//                    }
+        //reads user input.
+        readUserInput();
+    }
 
-                    list.add(kbd.nextInt());
-                    kbd.nextLine();
-                    int i = 0;
-//                    for (int i = 0; i < list.size() - 1; i++) {
-                    if (list.get(i) <= list.get(i++)) {
-                        System.out.println("That list is sorted.");
-                    } else {
-                        System.out.println("Out of order");
-                    }
-                    i++;
-//                    }
+    /**
+     * Printing out some messages to explain what is the program about with the
+     * author name
+     */
+    private static void printIntroduction() {
+        //The program name.
+        System.out.println("Command Loop\n"
+                + "---------------------\n");
+        //Say the author name.
+        System.out.println("by Andrew Boutres (A00428722)\n");
+
+        //Describe the program
+        System.out.println("This program computes commands using a method "
+                + "called command loop\n");
+
+    }
+
+    /**
+     * A method that reads the user input.
+     */
+    private static void readUserInput() {
+        boolean quit = false;
+        do {
+            Scanner kbd = new Scanner(System.in);
+            System.out.print(">>> ");
+            String[] request = kbd.nextLine().split("\\s");
+            String command = request[0];
+
+            switch (command) {
+                case "help":
+                    giveInstructions();
+                    break;
+                case "factorial":
+                    findFactorial(request);
+                    break;
+                case "sorted":
+                    // sayIfSorted(request);
+                    break;
+                case "gcd":
+                    findGcd(request, kbd);
+                    break;
+                case "quit":
+                    quit = true;
+                    System.out.println("\nGood-bye.");
                     break;
 
-                }
             }
+        } while (!quit);
+    }
 
-            System.out.print(">>> ");
-            result = kbd.next();
+    private static void giveInstructions() {
+        System.out.println("\n\nValid commands are:\n"
+                + " - factorial #\n"
+                + "     The product of all numbers from 1 to #.\n"
+                + "     (The argument must not be negative.)\n"
+                + "\n"
+                + " - gcd # #\n"
+                + "     The greatest common divisor of the two numbers.\n"
+                + "     The biggest number that divides evenly "
+                + "into both of them.\n"
+                + "\n"
+                + " - sorted #...\n"
+                + "     Whether the numbers are in order "
+                + "from smallest to largest.\n"
+                + "     If not, then where the first out-of-order number is.\n"
+                + "\n"
+                + " - help\n"
+                + "     This help message.\n"
+                + "\n"
+                + " - quit\n"
+                + "     End the program.\n\n");
+
+    }
+
+    /**
+     * its a factorial command that gets the factorial
+     *
+     * @param s1
+     */
+    private static void findFactorial(String[] s1) {
+        //declare variables
+        int factorial = 1, number;
+        number = Integer.parseInt(s1[1]);
+
+        for (int j = 1; j <= number; j++) {
+            factorial = factorial * j;
         }
+        System.out.println("\n" + number + "! == " + factorial + "\n");
     }
-
-    private static void printIntroduction() {
-        System.out.println("Assignment 'A09', Due: 2019-03-29, CSCI: 1228\n"
-                + "---------------------------------------------\n"
-                + "by Aitezaz Siddiqi(A00431079)\n"
-                + "---------------------------------------------\n"
-                + "This program reads and answers requests from \nthe user. "
-                + "Errors in the requests are handled\nusing exceptions without"
-                + " ending the program.\n"
-                + "---------------------------------------------\n");
+    /**
+     * A method that finds the great common divisor 
+     * @param b
+     * @param kbd 
+     */
+    private static void findGcd(String[] b, Scanner kbd) {
+        
+        
     }
+    /**
+     * 
+     * @param num1
+     * @param num2
+     * @return 
+     */
+    private static int itCalculateGcd(int num1, int num2) {
 
-    private static int getFactorial(int number) {
-        int result = 0;
-
-        if (number == 0 || number == 1) {
-            return 1;
-        } else if (number < 0) {
-            System.out.println("Undefined");
+        if (num2 == 0) {
+            return num1;
         } else {
-            result = number * getFactorial(number - 1);
+            return itCalculateGcd(num2, num1 % num2);
         }
-        return result;
+//       gcd 
+
     }
-
-    private static int greatestCommonDivisor(int number1, int number2) {
-        int result;
-        if (number2 == 0) {
-            return number1;
-        } else {
-            result = greatestCommonDivisor(number2, number1 % number2);
-        }
-
-        return result;
-    }
-
 }
